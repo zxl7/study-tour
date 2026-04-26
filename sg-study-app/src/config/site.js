6f5cba64bfe7b9f690eacc2e58a6e7c8958353a9
zxl7 <zxlcoder@gmail.com>
2026-04-26 19:05:33 +0800

    style(config): 统一站点配置文件中的引号和缩进格式
    将双引号替换为单引号以保持一致性，并修正缩进为两个空格。同时格式化顾问 Joey 的电话号码，增加空格以提高可读性。

--- sg-study-app/src/config/site.js
+++ sg-study-app/src/config/site.js
@@ -6,24 +6,23 @@
  */
 
 export const SITE = Object.freeze({
-	brandCN: '盛昌利民 · 狮城研学',
-	brandEN: 'Shengchang Limin · Singapore Study Tour',
-	companyCN: '海南盛昌利民传媒有限公司',
+  brandCN: "盛昌利民 · 狮城研学",
+  brandEN: "Shengchang Limin · Singapore Study Tour",
+  companyCN: "海南盛昌利民传媒有限公司",
 
-	hotline: '0898-65392581',
-	hotlineHint: '全国服务咨询热线',
+  hotline: "0898-65392581",
+  hotlineHint: "全国服务咨询热线",
 
-	// PC 首页「咨询报名」模块中的地址文案
-	chinaOfficeAddress:
-		'中国运营中心：海南省海口市美兰区国兴街道国兴大道15A号，全球贸易之窗9003室',
+  // PC 首页「咨询报名」模块中的地址文案
+  chinaOfficeAddress: "中国运营中心：海南省海口市美兰区国兴街道国兴大道15A号，全球贸易之窗9003室",
 
-	// PC「合作咨询」页中的地址/邮箱
-	bizEmail: 'shengchanglimin@163.com',
-	bizAddress: '海南省海口市美兰区国兴大道15A号全球贸易之窗30楼',
+  // PC「合作咨询」页中的地址/邮箱
+  bizEmail: "shengchanglimin@163.com",
+  bizAddress: "海南省海口市美兰区国兴大道15A号全球贸易之窗30楼",
 
-	advisors: [
-		{ name: 'Jason', phone: '166 8978 0303', qr: '/static/img/QR_Code-J.jpg' },
-		{ name: 'Adela', phone: '173 3083 5386', qr: '/static/img/QR_Code-A.jpg' },
-		{ name: 'Joey', phone: '19907618131', qr: '/static/img/QR_Code-O.jpg' },
-	],
+  advisors: [
+    { name: "Jason", phone: "166 8978 0303", qr: "/static/img/QR_Code-J.jpg" },
+    { name: "Adela", phone: "173 3083 5386", qr: "/static/img/QR_Code-A.jpg" },
+    { name: "Joey", phone: "199 0761 8131", qr: "/static/img/QR_Code-O.jpg" },
+  ],
 })
