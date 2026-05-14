3babd3a58c358c13b3ae8cc392216ab65036777d
zxl7 <zxlcoder@gmail.com>
2026-05-14 23:03:40 +0800

    chore: 更新小程序配置、隐私页面与相关素材
    完善小程序 manifest.json 的基础信息，调整项目构建忽略文件列表，将隐私页面标题标签从text改为p优化语义，更新顾问二维码图片并清理 .DS_Store 系统文件

--- sg-study-app/src/pages/privacy/index.vue
+++ sg-study-app/src/pages/privacy/index.vue
@@ -10,22 +10,22 @@
 				</view>
 
 				<view class="block">
-					<text class="sg-doc-h">信息收集</text>
+					<p class="sg-doc-h">信息收集</p>
 					<text class="sg-doc-p">我们仅在您主动提交咨询表单、评估需求时收集必要的个人信息（如姓名、联系方式、孩子年龄、教育背景等），用于为您提供定制化的服务方案。</text>
 				</view>
 
 				<view class="block">
-					<text class="sg-doc-h">信息使用</text>
+					<p class="sg-doc-h">信息使用</p>	
 					<text class="sg-doc-p">您的个人信息将被严格保密，仅限于内部顾问团队进行业务评估与联络。我们承诺，绝不向任何第三方出售、出租或交易您的个人数据。</text>
 				</view>
 
 				<view class="block">
-					<text class="sg-doc-h">数据安全</text>
+					<p class="sg-doc-h">数据安全</p>
 					<text class="sg-doc-p">我们采用行业标准的安全技术和内部管理制度，防止您的个人信息泄露、毁损或丢失。所有涉及核心身份规划的材料均受严格的保密协议(NDA)保护。</text>
 				</view>
 
 				<view class="block">
-					<text class="sg-doc-h">您的权利</text>
+					<p class="sg-doc-h">您的权利</p>
 					<text class="sg-doc-p">您有权随时要求查看、修改或删除我们在系统中保存的您的个人信息。如需操作，请联系我们的客服专员。</text>
 				</view>
 			</view>
